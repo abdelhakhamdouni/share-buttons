46ae99afd98732b58af41618071c0d6b0a6115c0
abdelhak hamdouni <abdelhakhamdouni@sfr.fr>
2020-12-18 10:37:59 +0100

    transfert the project on github

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# Pourquoi créer ses propres bouton de partage ?
+
+Certes, il en existe plusieurs librairies et plugins pour rendre et intégrer des boutons de partage sur son site web, tel que ShareThis ou autre. Cette approche présente plusieurs avantages, après la création de votre compte et le choix des réglages vous obtiendrez un code à mettre sur votre site web, et le tour est joué. Mais aussi un inconvénient, vous restez lié à cette plateforme, et vous n’avez pas la main sur ce qui se passe sur votre site web. En plus pour apprendre comment sont créés ses boutons et comment se passe le partage on va tous coder a la main ou presque. 
+
---
+++ app.js
@@ -0,0 +1,61 @@
+(function(){
+
+    document.querySelector('#share-twitter').addEventListener('click', function(e){
+
+        e.preventDefault();
+
+        var urlAPartager = this.getAttribute('data-url');
+
+        var url = "https://twitter.com/intent/tweet?text=" + encodeURIComponent("titre du site")
+
+            "&url=" + encodeURIComponent(urlAPartager);
+
+            window.open(url, "Partager sur Twitter", width=640, height=320, top=100, left=100);
+
+    });
+
+ 
+
+    document.querySelector('#share-facebook').addEventListener('click', function(e){
+
+        e.preventDefault();
+
+        var urlAPartager = this.getAttribute('data-url');
+
+        var url = "https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(urlAPartager);
+
+         window.open(url, "Partager sur Twitter", width=640, height=320, top=100, left=100);
+
+    });
+
+ 
+
+    document.querySelector('#share-gplus').addEventListener('click', function(e){
+
+        e.preventDefault();
+
+        var urlAPartager = this.getAttribute('data-url');
+
+        var url = "https://plus.google.com/share?url=" + encodeURIComponent(urlAPartager);
+
+         window.open(url, "Partager sur Twitter", width=640, height=320, top=100, left=100);
+
+    });
+
+ 
+
+    document.querySelector('#share-linkedin').addEventListener('click', function(e){
+
+        e.preventDefault();
+
+        var urlAPartager = this.getAttribute('data-url');
+
+        var url = "https://www.linkedin.com/shareArticle?url=" + encodeURIComponent(urlAPartager);
+
+         window.open(url, "Partager sur Twitter", width=640, height=320, top=100, left=100);
+
+    });
+
+ 
+
+})();

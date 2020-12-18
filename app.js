e324d9d27d6ae0ec88d9c758a5aed17ed8de643f
abdelhak HAMDOUNI <51864438+abdelhakhamdouni@users.noreply.github.com>
2020-12-18 10:47:50 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,61 +1,76 @@
-(function(){
+(function () {
+  document
+    .querySelector("#share-twitter")
+    .addEventListener("click", function (e) {
+      e.preventDefault();
 
-    document.querySelector('#share-twitter').addEventListener('click', function(e){
+      var urlAPartager = this.getAttribute("data-url");
+      var url =
+        "https://twitter.com/intent/tweet?text=" +
+        encodeURIComponent("titre du site");
+      "&url=" + encodeURIComponent(urlAPartager);
 
-        e.preventDefault();
-
-        var urlAPartager = this.getAttribute('data-url');
-
-        var url = "https://twitter.com/intent/tweet?text=" + encodeURIComponent("titre du site")
-
-            "&url=" + encodeURIComponent(urlAPartager);
-
-            window.open(url, "Partager sur Twitter", width=640, height=320, top=100, left=100);
-
+      window.open(
+        url,
+        "Partager sur Twitter",
+        (width = 640),
+        (height = 320),
+        (top = 100),
+        (left = 100)
+      );
     });
 
- 
+  document
+    .querySelector("#share-facebook")
+    .addEventListener("click", function (e) {
+      e.preventDefault();
 
-    document.querySelector('#share-facebook').addEventListener('click', function(e){
+      var urlAPartager = this.getAttribute("data-url"),
+          url = "https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(urlAPartager);
 
-        e.preventDefault();
-
-        var urlAPartager = this.getAttribute('data-url');
-
-        var url = "https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(urlAPartager);
-
-         window.open(url, "Partager sur Twitter", width=640, height=320, top=100, left=100);
-
+      window.open(
+        url,
+        "Partager sur Twitter",
+        (width = 640),
+        (height = 320),
+        (top = 100),
+        (left = 100)
+      );
     });
 
- 
+  document
+    .querySelector("#share-gplus")
+    .addEventListener("click", function (e) {
+      e.preventDefault();
 
-    document.querySelector('#share-gplus').addEventListener('click', function(e){
+      var urlAPartager = this.getAttribute("data-url")
+          url = "https://plus.google.com/share?url=" + encodeURIComponent(urlAPartager);
 
-        e.preventDefault();
-
-        var urlAPartager = this.getAttribute('data-url');
-
-        var url = "https://plus.google.com/share?url=" + encodeURIComponent(urlAPartager);
-
-         window.open(url, "Partager sur Twitter", width=640, height=320, top=100, left=100);
-
+      window.open(
+        url,
+        "Partager sur Twitter",
+        (width = 640),
+        (height = 320),
+        (top = 100),
+        (left = 100)
+      );
     });
 
- 
+  document
+    .querySelector("#share-linkedin")
+    .addEventListener("click", function (e) {
+      e.preventDefault();
 
-    document.querySelector('#share-linkedin').addEventListener('click', function(e){
+      var urlAPartager = this.getAttribute("data-url"),
+          url = "https://www.linkedin.com/shareArticle?url=" + encodeURIComponent(urlAPartager);
 
-        e.preventDefault();
-
-        var urlAPartager = this.getAttribute('data-url');
-
-        var url = "https://www.linkedin.com/shareArticle?url=" + encodeURIComponent(urlAPartager);
-
-         window.open(url, "Partager sur Twitter", width=640, height=320, top=100, left=100);
-
+      window.open(
+        url,
+        "Partager sur Twitter",
+        (width = 640),
+        (height = 320),
+        (top = 100),
+        (left = 100)
+      );
     });
-
- 
-
 })();
